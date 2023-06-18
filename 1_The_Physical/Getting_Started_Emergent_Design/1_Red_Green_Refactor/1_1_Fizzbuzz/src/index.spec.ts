a8b70832535277f76cb511f475f6b9271b4e858a
krzysztofbogusz91 <bogusz.web@gmail.com>
2023-06-18 19:45:55 +0200

    [green] 3 passes

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("when passed 3 passes fizz", () => {
+    const result = fizzBuzz(3);
+    expect(result).toEqual("fizz");
+  });
 });
