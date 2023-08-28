5534bc9786fec18c7fa2c765989a4475edb9f531
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-08-28 20:16:03 +0200

    [red] init

---
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export const fizzbuzz = (n: number): string => {
+  return "fizz";
+};
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,19 @@
+import { fizzbuzz } from "./fizzbuzz";
+
+// each return value is a string
+// 3 returns "Fizz"
+// 5 returns "Buzz
+// 15 returns "FizzBuzz"
+// 9 returns "Fizz"
+// 43 returns "43"
+// 42 returns "Fizz"
+// 45 returns "FizzBuzz"
+// 102 (you decide, throw an Error or handle some other way)
+// -12 (you decide, throw an Error or handle some other way)
+// any non-number (you decide, throw an Error or handle some other way)
 
 describe("fizzbuzz", () => {
-
-
+  it("should print fizzbuzz", () => {
+    expect(fizzbuzz(15)).toEqual("fizzbuzz");
+  });
 });
