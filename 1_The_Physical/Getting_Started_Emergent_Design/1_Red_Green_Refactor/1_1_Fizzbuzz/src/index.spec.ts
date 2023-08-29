2b40320cf718ae77305d8e11805ecf1716acb8d1
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-08-29 20:37:51 +0200

    [refactor] adds more cases for not a number

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,17 +1,5 @@
 import { fizzbuzz } from "./fizzbuzz";
 
-// each return value is a string
-// 3 returns "Fizz"
-// 5 returns "Buzz
-// 15 returns "FizzBuzz"
-// 9 returns "Fizz"
-// 43 returns "43"
-// 42 returns "Fizz"
-// 45 returns "FizzBuzz"
-// 102 (you decide, throw an Error or handle some other way)
-// -12 (you decide, throw an Error or handle some other way)
-// any non-number (you decide, throw an Error or handle some other way)
-
 describe("fizzbuzz", () => {
   it.each([3, 9, 42])("should return Fizz when given %i", (n) => {
     expect(fizzbuzz(n)).toEqual("Fizz");
@@ -27,6 +15,10 @@ describe("fizzbuzz", () => {
 
   it.each([1, 2, 2224])("should return %i when given %i", (n) => {
     expect(fizzbuzz(n)).toEqual(n.toString());
+  });
+
+  it.each([undefined, null, {}])("should throw an error when given %i", (n) => {
+    expect(fizzbuzz(n as any)).toEqual(n);
   });
 
   it("should return Fizz for -12", () => {
