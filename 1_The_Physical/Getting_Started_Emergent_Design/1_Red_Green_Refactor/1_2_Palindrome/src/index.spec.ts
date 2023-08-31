f87d6fa6f19a0e2e2cea94d0819f9f030e08f561
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-08-31 20:41:52 +0200

    [red] mom shuold return true

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,16 @@
+// "mom" returns true
+// "Mom" returns true
+// "MoM" returns true
+// "Momx" returns false
+// "xMomx" returns true
+// "Was It A Rat I Saw" returns true
+// "Never Odd or Even" returns true
+// "Never Odd or Even1" returns false
+// "1Never Odd or Even1" returns true
+import { palindrome } from "./index";
 
-describe('palindrome checker', () => {
-
-})
+describe("palindrome checker", () => {
+  it("'mom' returns true", () => {
+    expect(palindrome("mom")).toBe(true);
+  });
+});
---
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -0,0 +1,1 @@
+export const palindrome = (input: string): boolean => {};
