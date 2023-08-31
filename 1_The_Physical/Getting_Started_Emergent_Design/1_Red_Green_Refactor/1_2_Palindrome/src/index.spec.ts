2cf7b452233b0b70a1195b149c317036bea2a6d8
Krzysztof Bogusz <bogusz.web@gmail.com>
2023-08-31 20:54:39 +0200

    [refactor] removes comment

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,12 +1,3 @@
-// "mom" returns true
-// "Mom" returns true
-// "MoM" returns true
-// "Momx" returns false
-// "xMomx" returns true
-// "Was It A Rat I Saw" returns true
-// "Never Odd or Even" returns true
-// "Never Odd or Even1" returns false
-// "1Never Odd or Even1" returns true
 import { palindrome } from "./index";
 
 describe("palindrome checker", () => {
